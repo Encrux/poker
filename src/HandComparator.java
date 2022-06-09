983fc993c22d080bad19e7cc1ed1d7cdc96da8a9
Daniel <boesch.daniel98@gmail.com>
2022-06-09 10:32:07 +0200

    Add hand comparator edge case stubs

--- src/HandComparator.java
+++ src/HandComparator.java
@@ -11,6 +11,63 @@ public class HandComparator {
     }
 
     public Winner determineWinner() {
+        final var hand1Class = hand1.getHandClass();
+        final var hand2Class = hand2.getHandClass();
+
+        if (hand1Class.ordinal() > hand2Class.ordinal()) {
+            return Winner.HAND1;
+        }
+
+        if (hand2Class.ordinal() > hand1Class.ordinal()) {
+            return Winner.HAND2;
+        }
+
+        return switch (hand1Class) {
+            case HighCard -> compareHighCard();
+            case Pair -> comparePair();
+            case TwoPairs -> compareTwoPairs();
+            case ThreeOfAKind -> compareThreeOfAKind();
+            case Straight -> compareStraight();
+            case Flush -> compareFlush();
+            case FullHouse -> compareFullHouse();
+            case FourOfAKind -> compareFourOFAKind();
+            case StraightFlush -> compareStraightFlush();
+        };
+    }
+
+    private Winner compareHighCard() {
+        return null;
+    }
+
+    private Winner comparePair() {
+        return null;
+    }
+
+    private Winner compareTwoPairs() {
+        return null;
+    }
+
+    private Winner compareThreeOfAKind() {
+        return null;
+    }
+
+    private Winner compareStraight() {
+        return null;
+    }
+
+    private Winner compareFlush() {
+        return null;
+    }
+
+    private Winner compareFullHouse() {
+        return null;
+    }
+
+    private Winner compareFourOFAKind() {
+        return null;
+    }
+
+    private Winner compareStraightFlush() {
         return null;
     }
 }
