09fd17465cb7d83317e3d301a1a2423eaf2dee5b
Daniel <boesch.daniel98@gmail.com>
2022-06-09 09:50:51 +0200

    Add hand comparator stub

---
+++ src/HandComparator.java
@@ -0,0 +1,16 @@
+import model.Winner;
+
+public class HandComparator {
+
+    private final Hand hand1;
+    private final Hand hand2;
+
+    public HandComparator(final Hand hand1, final Hand hand2) {
+        this.hand1 = hand1;
+        this.hand2 = hand2;
+    }
+
+    public Winner determineWinner() {
+        return null;
+    }
+}
---
+++ src/model/Winner.java
@@ -0,0 +1,10 @@
+package model;
+
+public enum Winner {
+    HAND1,
+    HAND2,
+    /**
+     * Ties can occur because Suits are unordered.
+     */
+    TIE
+}
