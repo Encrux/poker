27e5a344f49cba81a128653bf9716f9ae65a2ac0
Daniel <boesch.daniel98@gmail.com>
2022-06-09 13:06:33 +0200

    Add HandComparator logic

--- src/HandComparator.java
+++ src/HandComparator.java
@@ -1,4 +1,9 @@
+import model.Card;
 import model.Winner;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HandComparator {
 
@@ -11,8 +16,12 @@ public class HandComparator {
     }
 
     public Winner determineWinner() {
-        final var hand1Class = hand1.getHandClass();
-        final var hand2Class = hand2.getHandClass();
+        return determineWinner(hand1.getCards(), hand2.getCards());
+    }
+
+    private Winner determineWinner(final List<Card> cards1, final List<Card> cards2) {
+        final var hand1Class = Hand.getHandClass(cards1);
+        final var hand2Class = Hand.getHandClass(cards2);
 
         if (hand1Class.ordinal() > hand2Class.ordinal()) {
             return Winner.HAND1;
@@ -23,51 +32,159 @@ public class HandComparator {
         }
 
         return switch (hand1Class) {
-            case HighCard -> compareHighCard();
-            case Pair -> comparePair();
-            case TwoPairs -> compareTwoPairs();
-            case ThreeOfAKind -> compareThreeOfAKind();
-            case Straight -> compareStraight();
-            case Flush -> compareFlush();
-            case FullHouse -> compareFullHouse();
-            case FourOfAKind -> compareFourOFAKind();
-            case StraightFlush -> compareStraightFlush();
+            case HighCard -> compareHighCard(cards1, cards2);
+            case Pair -> comparePair(cards1, cards2);
+            case TwoPairs -> compareTwoPairs(cards1, cards2);
+            case ThreeOfAKind -> compareThreeOfAKind(cards1, cards2);
+            case Straight -> compareStraight(cards1, cards2);
+            case Flush -> compareFlush(cards1, cards2);
+            case FullHouse -> compareFullHouse(cards1, cards2);
+            case FourOfAKind -> compareFourOFAKind(cards1, cards2);
+            case StraightFlush -> compareStraightFlush(cards1, cards2);
         };
     }
 
-    private Winner compareHighCard() {
-        return null;
+    /**
+     * Highest card wins.
+     */
+    private Winner compareHighCard(List<Card> hand1Cards, List<Card> hand2Cards) {
+        final var hand1Highest = Collections.max(hand1Cards);
+        final var hand2Highest = Collections.max(hand2Cards);
+
+        if (hand1Highest.compareTo(hand2Highest) > 0) {
+            return Winner.HAND1;
+        }
+
+        if (hand2Highest.compareTo(hand1Highest) > 0) {
+            return Winner.HAND2;
+        }
+
+        return Winner.TIE;
     }
 
-    private Winner comparePair() {
-        return null;
+    /**
+     * Higher pair wins.
+     */
+    private Winner comparePair(List<Card> hand1Cards, List<Card> hand2Cards) {
+        return compareNOfAKind(hand1Cards, hand2Cards, 2);
     }
 
-    private Winner compareTwoPairs() {
-        return null;
+    /**
+     * The higher ranked pair wins.
+     */
+    private Winner compareTwoPairs(List<Card> hand1Cards, List<Card> hand2Cards) {
+        final var hand1GroupedByValue = Hand.groupCardsByValue(hand1Cards);
+        final var hand2GroupedByValue = Hand.groupCardsByValue(hand2Cards);
+
+        final var hand1Pairs = hand1GroupedByValue.keySet().stream()
+                .filter(key -> hand1GroupedByValue.get(key).size() == 2)
+                .collect(Collectors.toList());
+
+        final var hand2Pairs = hand2GroupedByValue.keySet().stream()
+                .filter(key -> hand2GroupedByValue.get(key).size() == 2)
+                .collect(Collectors.toList());
+
+        //this is a mess
+        final var hand1BiggerPair = compareHighCard(
+                hand1GroupedByValue.get(hand1Pairs.get(0)),
+                hand1GroupedByValue.get(hand1Pairs.get(1))
+        ) == Winner.HAND1 ? hand1GroupedByValue.get(hand1Pairs.get(0))
+                : hand1GroupedByValue.get(hand1Pairs.get(1));
+
+        final var hand2BiggerPair = compareHighCard(
+                hand2GroupedByValue.get(hand2Pairs.get(0)),
+                hand2GroupedByValue.get(hand2Pairs.get(1))
+        ) == Winner.HAND1 ? hand1GroupedByValue.get(hand1Pairs.get(0))
+                : hand2GroupedByValue.get(hand2Pairs.get(1));
+
+        final var highestPairWinner = compareHighCard(hand1BiggerPair, hand2BiggerPair);
+        if (highestPairWinner != Winner.TIE) {
+            return highestPairWinner;
+        }
+
+        //handle kicker
+        hand1Cards.remove(hand1BiggerPair);
+        hand2Cards.remove(hand2BiggerPair);
+
+        return determineWinner(hand1Cards, hand2Cards);
     }
 
-    private Winner compareThreeOfAKind() {
-        return null;
+    /**
+     * The higher ranking card wins.
+     */
+    private Winner compareThreeOfAKind(List<Card> hand1Cards, List<Card> hand2Cards) {
+        return compareNOfAKind(hand1Cards, hand2Cards, 3);
     }
 
-    private Winner compareStraight() {
-        return null;
+    /**
+     * The straight with the highest ranking card wins.
+     */
+    private Winner compareStraight(List<Card> hand1Cards, List<Card> hand2Cards) {
+        return compareHighCard(hand1Cards, hand2Cards);
     }
 
-    private Winner compareFlush() {
-        return null;
+    /**
+     * The flush with the highest ranking card wins.
+     */
+    private Winner compareFlush(List<Card> hand1Cards, List<Card> hand2Cards) {
+        return compareHighCard(hand1Cards, hand2Cards);
     }
 
-    private Winner compareFullHouse() {
-        return null;
+    /**
+     * The hand with the higher ranking set of three cards wins.
+     */
+    private Winner compareFullHouse(List<Card> hand1Cards, List<Card> hand2Cards) {
+        return compareNOfAKind(hand1Cards, hand2Cards, 3);
     }
 
-    private Winner compareFourOFAKind() {
-        return null;
+    /**
+     * The higher ranked set of four cards wins.
+     */
+    private Winner compareFourOFAKind(List<Card> hand1Cards, List<Card> hand2Cards) {
+        return compareNOfAKind(hand1Cards, hand2Cards, 4);
     }
 
-    private Winner compareStraightFlush() {
-        return null;
+    /**
+     * Higher straight flush wins.
+     */
+    private Winner compareStraightFlush(List<Card> hand1Cards, List<Card> hand2Cards) {
+        return compareHighCard(hand1.getCards(), hand2.getCards());
+    }
+
+    private Winner compareNOfAKind(final List<Card> hand1Cards, final List<Card> hand2Cards, final int n) {
+        final var hand1GroupedByValue = Hand.groupCardsByValue(hand1Cards);
+        final var hand2GroupedByValue = Hand.groupCardsByValue(hand2Cards);
+
+        final var match1 = hand1GroupedByValue
+                .values().stream()
+                .filter(group -> group.size() == n)
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+
+        final var match2 = hand2GroupedByValue
+                .values().stream()
+                .filter(group -> group.size() == n)
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+
+        final var highCardResult = compareHighCard(match1, match2);
+        if (highCardResult != Winner.TIE) {
+            return highCardResult;
+        }
+
+        //Kicker
+        final var rest1 = hand1GroupedByValue
+                .values().stream()
+                .filter(group -> group.size() != n)
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+
+        final var rest2 = hand2GroupedByValue
+                .values().stream()
+                .filter(group -> group.size() != 4)
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+
+        return determineWinner(rest1, rest2);
     }
 }
